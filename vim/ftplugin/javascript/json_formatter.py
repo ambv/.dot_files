fe7216ce0cef81e886953ffddbd52c11525730e4
Łukasz Langa <lukasz@langa.pl>
2010-02-02 01:31:10 +0100

    vim: warning if the user doesn't have simplejson or Python 2.6+

--- vim/ftplugin/javascript/json_formatter.py
+++ vim/ftplugin/javascript/json_formatter.py
@@ -9,19 +9,21 @@ except ImportError:
         import json
     except ImportError:
         imports_ok = False
+
+import vim
+
+def ReformatJSON():
+    if not imports_ok:
         print "JSON formatter requires simplejson or Python 2.6+."
+        return
 
-if imports_ok:
-    import vim
-
-    def ReformatJSON():
-        try:
-            json_string = json.loads('\n'.join(vim.current.buffer))
-            vim.current.buffer[:] = json.dumps(json_string, indent=2).split('\n')
-        except ValueError, ve:
-            message = str(ve)
-            positions = position_regexp.search(message)
-            if positions:
-                vim.command('norm ' + positions.group(1) + 'G')
-                vim.command('norm ' + str(int(positions.group(2)) + 1) + '|')
-            print message
+    try:
+        json_string = json.loads('\n'.join(vim.current.buffer))
+        vim.current.buffer[:] = json.dumps(json_string, indent=2).split('\n')
+    except ValueError, ve:
+        message = str(ve)
+        positions = position_regexp.search(message)
+        if positions:
+            vim.command('norm ' + positions.group(1) + 'G')
+            vim.command('norm ' + str(int(positions.group(2)) + 1) + '|')
+        print message
