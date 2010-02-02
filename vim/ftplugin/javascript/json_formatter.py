5f65f8626403791650d888104f8a70e365fdecff
Łukasz Langa <lukasz@langa.pl>
2010-02-02 01:31:10 +0100

    vim: warning if the user doesn't have simplejson or Python 2.6+

--- vim/ftplugin/javascript/json_formatter.py
+++ vim/ftplugin/javascript/json_formatter.py
@@ -1,6 +1,6 @@
-import sys
 import re
 position_regexp = re.compile(r'line (\d+) column (\d+)')
+imports_ok = True
 
 try:
     import simplejson as json
@@ -8,19 +8,20 @@ except ImportError:
     try:
         import json
     except ImportError:
+        imports_ok = False
         print "JSON formatter requires simplejson or Python 2.6+."
-        sys.exit(1)
 
-import vim
+if imports_ok:
+    import vim
 
-def ReformatJSON():
-    try:
-        json_string = json.loads('\n'.join(vim.current.buffer))
-        vim.current.buffer[:] = json.dumps(json_string, indent=2).split('\n')
-    except ValueError, ve:
-        message = str(ve)
-        positions = position_regexp.search(message)
-        if positions:
-            vim.command('norm ' + positions.group(1) + 'G')
-            vim.command('norm ' + str(int(positions.group(2)) + 1) + '|')
-        print message
+    def ReformatJSON():
+        try:
+            json_string = json.loads('\n'.join(vim.current.buffer))
+            vim.current.buffer[:] = json.dumps(json_string, indent=2).split('\n')
+        except ValueError, ve:
+            message = str(ve)
+            positions = position_regexp.search(message)
+            if positions:
+                vim.command('norm ' + positions.group(1) + 'G')
+                vim.command('norm ' + str(int(positions.group(2)) + 1) + '|')
+            print message
