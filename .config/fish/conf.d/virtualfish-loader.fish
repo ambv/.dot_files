fec8a122482a250c007e5919b864b5525f9a0cde
Łukasz Langa <lukasz@langa.pl>
2023-03-13 16:08:19 +0100

    Auto-activate venvs per directory

--- .config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,5 @@
 set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/opt/python@3.10/bin/python3.10
 source /opt/homebrew/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /opt/homebrew/lib/python3.10/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
